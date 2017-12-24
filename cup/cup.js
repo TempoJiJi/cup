6f5bb645101159db622efc38452871d11c0cc57e
tundergod <tundergod1882@gmail.com>
2017-12-24 11:40:57 +0000

    first commit

---
+++ cup/cup.js
@@ -0,0 +1,64 @@
+/*
+1. get advertisement from beacons using noble modules
+2. PUT(RESTful API) advertisement to server
+*/
+
+// module used
+var noble = require('noble')
+var request = require('request')
+
+var advData = require('./data.json')
+var method = 'PUT'
+var url = 'http://192.168.1.105:3000'
+var isDuplicate = true
+var options = {
+  headers: {'Connection': 'close'},
+  url: url,
+  method: method,
+  json: true,
+  body: advData
+}
+
+var cupID = 'cup001'
+
+// fe9a is the service uuid of estimote proximity beacon
+noble.on('stateChange', function (state) {
+  if (state === 'poweredOn') {
+    noble.startScanning('fe9a', isDuplicate)
+  } else {
+    noble.stopScanning()
+  }
+})
+
+var count = 0;
+
+//scan and send advertisement to the server
+noble.on('discover', function (peripheral) {
+  // send PUT request to specific URL
+
+  advData.serviceUUID = peripheral.id
+  advData.rssi = peripheral.rssi
+  advData.serviceData = peripheral.advertisement.serviceData
+  advData.cupID = cupID
+  request(options, callback)
+
+  console.log('Scanned and sending :\n\t ' + JSON.stringify(advData))
+  //console.log()
+})
+
+// get response if have any comment from web application
+// 200 OK, 201 CREATED, 202 ACCEPTED
+function callback (error, response, data) {
+  if (!error && response.statusCode === (200 || 202)) {
+    console.log('Info :\n', data)
+  }
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
